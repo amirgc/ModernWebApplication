1cd33b0af522de19b951c31cb511b69366eaf17f
amirgc <gcamir15@gmail.com>
2018-12-05 16:02:43 -0500

    lab 8 doing

--- lab8/MongoCrupApp/routes/index.js
+++ lab8/MongoCrupApp/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+"use strict";
+var express = require("express");
 var router = express.Router();
+const MongoCLient = require("mongodb").MongoClient;
+const client = new MongoCLient("mongodb://localhost:27017");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  client.connect(function(err) {
+    const db = client.db("rms");
+    db.collection("restaurants")
+      .find({}).project({ restaurant_id: 1, name: 1, district: 1, cuisine: 1,_id:0 })
+      .toArray(function(err, result) {
+        if (err) throw err;
+        res.send(result);
+      });
+  });
 });
 
 module.exports = router;
